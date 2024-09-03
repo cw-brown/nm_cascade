d04352686ded5e1560d683a46a4ee06413b094cd
cw-brown <calebwbhome@gmail.com>
2024-09-03 14:49:55 -0600

    Fixed errors, added more functionality

--- src/cascoptim.m
+++ src/cascoptim.m
@@ -1,19 +1,20 @@
-function [s, result, fval, flag] = cascoptim(sz, startfreq, stopfreq, nfreq, errfunc, ispar)
+function [s, result, fval, flag] = cascoptim(sz, startfreq, stopfreq, nfreq, errfunc, maxeval, ispar)
     %% Optimize the nxm grid.
     freqs = linspace(startfreq, stopfreq, nfreq);
     ports = fpg(sz); ports = ports(2:end-1);
     nvars = 2*sum((ports.^2+ports)/2)*nfreq;
     
-    if ispart == true
+    if ispar == true
         pool = parpool();
     end
     obj = @(x)cascadeobj(x, freqs, sz, errfunc);
     constr = @(x)lprestr(x, freqs, sz);
-    options = optimoptions("fmincon", "Display", "none", "MaxFunctionEvaluations", 1e5, "UseParallel", ispar);
+    options = optimoptions("fmincon", "Display", "none", "MaxFunctionEvaluations", maxeval, "UseParallel", ispar);
     lb = -ones(1, nvars); ub = ones(1, nvars);
     [result, fval, flag, ~] = fmincon(obj, rand(1, nvars), [], [], [], [], lb, ub, constr, options);
-    s = l2casc(num2sparam(result, freqs, sz), freqs, sz);
-    if ispart == true
+    s_params = l2casc(num2sparam(result, freqs, sz), freqs, sz);
+    s = sparameters(s_params, freqs);
+    if ispar == true
         delete(pool);
     end
 end
