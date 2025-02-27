749fc2c5b153720051cf8ad74942f169e8a6bdc4
unknown <santasmwlab@gmail.com>
2025-02-27 14:19:17 -0700

    New changes to reflect proper transfer function.

--- src/Synthesisv2/mlsynth2.m
+++ src/Synthesisv2/mlsynth2.m
@@ -2,34 +2,27 @@
 clear; clc; close all;
 %% Circuit 1
 cir1 = circuit('Circuit1');
-add(cir1, [1, 2], capacitor(10e-6));
-add(cir1, [2, 3], inductor(1e-6));
-add(cir1, [3, 4], inductor(68e-6));
-add(cir1, [3, 4], capacitor(22e-6));
-add(cir1, [3, 5], capacitor(1e-6));
-add(cir1, [4, 6], inductor(22e-6));
-setports(cir1, [1, 4], [5, 6]);
+nelem = 2;
+stx = rand(1, nelem);
+add(cir1, [1 2], capacitor(stx(1)));
+% add(cir1, [3 4], capacitor(stx(2)));
+setports(cir1, [1, 3], [2, 3]);
 
-freqs = logspace(3, 4, 100);
-s1 = sparameters(cir1, freqs);
-figure;
+freqs = linspace(0, 1, 200);
+s1 = sparameters(cir1, freqs); figure;
 plot(freqs, db(squeeze(s1.Parameters(1,1,:))), freqs, db(squeeze(s1.Parameters(1,2,:))), ...
     freqs, db(squeeze(s1.Parameters(2,1,:))), freqs, db(squeeze(s1.Parameters(2,2,:)))); grid on;
 legend('db(S_1_1)', 'db(S_1_2)', 'db(S_2_1)', 'db(S_2_2)');
 xlabel('Frequency (Hz)'); ylabel('Magnitude (dB)'); title('Circuit 1');
 
 %% Circuit 2
-cir2 = circuit('Circuit2');
-add(cir2, [1, 2], capacitor(68e-6));
-add(cir2, [1, 3], inductor(22e-6));
-add(cir2, [1, 3], capacitor(22e-6));
-add(cir2, [3, 2], capacitor(10e-6));
-add(cir2, [3, 4], inductor(1e-6));
-add(cir2, [2, 4], capacitor(68e-6));
-setports(cir2, [1, 2], [3, 4]);
+cir2 = circuit('Circuit1');
+nelem = 2;
+stx = rand(1, nelem);
+add(cir2, [1 2], inductor(stx(1)));
+setports(cir2, [1, 3], [2, 3]);
 
-s2 = sparameters(cir2, freqs);
-figure;
+s2 = sparameters(cir2, freqs); figure;
 plot(freqs, db(squeeze(s2.Parameters(1,1,:))), freqs, db(squeeze(s2.Parameters(1,2,:))), ...
     freqs, db(squeeze(s2.Parameters(2,1,:))), freqs, db(squeeze(s2.Parameters(2,2,:)))); grid on;
 legend('db(S_1_1)', 'db(S_1_2)', 'db(S_2_1)', 'db(S_2_2)');
@@ -48,17 +41,20 @@ sz = [1, 2, 1, 2];
 networks = fpg(sz);
 err = [squeeze(abs(s.Parameters(1, 1, :))) ...
        squeeze(abs(s.Parameters(1, 2, :))) ...
-       squeeze(abs(s.Parameters(2, 2, :)))];
+       squeeze(abs(s.Parameters(2, 2, :))) ...
+       squeeze(angle(s.Parameters(1, 1, :))) ...
+       squeeze(angle(s.Parameters(1, 2, :))) ...
+       squeeze(angle(s.Parameters(2, 2, :)))];
 
 obj = @(x, xdata) synobj(x, sz, xdata);
 constr = @(x) synconstr(x, sz);
 
-nvar = 25*sum(networks.^2+networks)/2;
-stx = 1e7*rand(1, nvar);
+nvar = 3*sum(networks.^2+networks)/2;
+stx = 2e-2*rand(1,nvar)-1e-2;
 
 options = optimoptions("lsqcurvefit","UseParallel",true,"PlotFcn",...
     ["optimplotx","optimplotfval","optimplotfirstorderopt"],...
-    "MaxFunctionEvaluations",3e4,"TypicalX",repmat(1e6, size(stx)));
+    "MaxFunctionEvaluations",3e4,"Display","iter");
 [solution,objectiveValue] = lsqcurvefit(obj,stx,freqs,err,[],[],[],[], ...
     [],[],constr,options);
 
@@ -71,19 +67,19 @@ for ii = 1:4
     [row, col] = ind2sub([2, 2], ii);
     subplot(2, 2, ii);
     plot(freqs, reshape(db(s.Parameters(row, col, :)), 1, []), freqs, reshape(db(F.ResponseData(row, col, :)), 1, []));
-    xscale('log'); xlabel('Frequency (Hz)'); ylabel('Magnitude (dB)');
+    xscale('linear'); xlabel('Frequency (Hz)'); ylabel('Magnitude (dB)');
     % legend('Input', 'Synthesis');
     title(['S_' num2str(col) '_' num2str(row)]); grid on;
 end
-figure;
-for ii = 1:4
-    [row, col] = ind2sub([2, 2], ii);
-    subplot(2, 2, ii);
-    plot(freqs, reshape(angle(s.Parameters(row, col, :)), 1, []), freqs, reshape(angle(F.ResponseData(row, col, :)), 1, []));
-    xscale('log'); xlabel('Frequency (Hz)'); ylabel('Phase');
-    % legend('Input', 'Synthesis');
-    title(['S_' num2str(col) '_' num2str(row)]); grid on;
-end
+% figure;
+% for ii = 1:4
+%     [row, col] = ind2sub([2, 2], ii);
+%     subplot(2, 2, ii);
+%     plot(freqs, reshape(angle(s.Parameters(row, col, :)), 1, []), freqs, reshape(angle(F.ResponseData(row, col, :)), 1, []));
+%     xscale('log'); xlabel('Frequency (Hz)'); ylabel('Phase');
+%     % legend('Input', 'Synthesis');
+%     title(['S_' num2str(col) '_' num2str(row)]); grid on;
+% end
 figure;
 for ii = 1:sz(1)*sz(2)
     subplot(sz(1), sz(2), ii);
