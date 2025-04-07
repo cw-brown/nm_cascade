7e20a6007964c2e628311ed14a6a954ffb03f691
cw-brown <calebwbhome@gmail.com>
2025-04-07 12:09:16 -0600

    Final commit, finally can clean and reorganize for final release

--- src/Synthesisv2/mlsynth5.m
+++ src/Synthesisv2/mlsynth5.m
@@ -1,13 +1,53 @@
 %% Setup for synthesis
-clear; clc; close all;
-rng(123456);
+% clear; clc; close all;
+% rng(17450);
+close all;
 
-sc = sparameters('bandpass.s2p');
-nfreqs = length(sc.Frequencies);
-freqs = linspace(0,1,length(sc.Frequencies));
-sc.Frequencies = freqs;
-xlims = [0, 1];
+% nfreqs = 200;
+% freqs = linspace(0.1,1,nfreqs);
+% magpart = cos(2*pi*freqs);
+% phasepart = cos(2*pi*freqs+deg2rad(-90));
+% s11 = magpart.*exp(1j*phasepart);
+% s22 = s11;
+% s12 = sqrt(1-magpart.^2).*exp(1j*(phasepart-pi/2));
+% s21 = s12;
+% s = zeros(2,2,nfreqs);
+% s(1,1,:) = s11;
+% s(1,2,:) = s12;
+% s(2,1,:) = s21;
+% s(2,2,:) = s22;
+% sc = sparameters(s,freqs);
+% xlims = [min(freqs), 1];
+% ylims = [-70, 10];
+
+sc = sparameters('lowpass.s2p');
+freqs = sc.Frequencies / max(sc.Frequencies);
+xlims = [min(freqs), 1];
 ylims = [-70, 10];
+
+% c = circuit();
+% add(c, [1 2], capacitor(7.45));
+% add(c, [2 3], inductor(2.25));
+% add(c, [3 4], capacitor(1));
+% add(c, [2 3], resistor(0.1));
+% add(c, [2 5], inductor(0.5));
+% add(c, [3 5], capacitor(0.25));
+% add(c, [3 5], inductor(0.75));
+% add(c, [1 5], capacitor(1));
+% add(c, [1 4], inductor(0.95));
+% setports(c, [1 4], [3 5]);
+% freqs = linspace(0.1, 1, 200);
+% sc = sparameters(c, freqs);
+% sc.Parameters(1,2,:) = sc.Parameters(1,2,:)*0.5e1;
+% sc.Parameters(2,1,:) = sc.Parameters(2,1,:)*0.5e1;
+% xlims = [min(freqs), 1];
+% ylims = [-70, 10];
+
+% T = readtable('bfp540_sopt.csv');
+% freqs = T.freq;
+% freqs = freqs / max(freqs);
+% xlims = [min(freqs), 1];
+% ylims = [-70, 5];
 
 figure('Name','Cascaded Network'); subplot(2,2,1);
 plot(freqs, real(squeeze(sc.Parameters(1,1,:))), freqs, real(squeeze(sc.Parameters(1,2,:))), ...
@@ -25,58 +65,54 @@ legend('S_1_1', 'S_1_2', 'S_2_1', 'S_2_2');
 sgtitle('Desired Network'); drawnow;
 
 %% Optimization
-err = [real(squeeze(sc.Parameters(1,1,:)))'...
-       real(squeeze(sc.Parameters(1,2,:)))'...
-       real(squeeze(sc.Parameters(2,2,:)))'...
-       imag(squeeze(sc.Parameters(1,1,:)))'...
-       imag(squeeze(sc.Parameters(1,2,:)))'...
-       imag(squeeze(sc.Parameters(2,2,:)))'];
+% err = [real(squeeze(sc.Parameters(1,1,:)))'...
+%        real(squeeze(sc.Parameters(1,2,:)))'...
+%        real(squeeze(sc.Parameters(2,2,:)))'...
+%        imag(squeeze(sc.Parameters(1,1,:)))'...
+%        imag(squeeze(sc.Parameters(1,2,:)))'...
+%        imag(squeeze(sc.Parameters(2,2,:)))'];
+err = [real(squeeze(sc.Parameters(1,2,:)))'...
+       imag(squeeze(sc.Parameters(1,2,:)))'];
 
 % Automatically find the best grid size that can match the target
 eval = [2,16,20,28,40,44,52,64,68,76,88,92,100,112,116,124,136,140,148,160,...
     164,172,184,188,196]; % for first order systems
 % eval = [4 28 40 56 76 88 104 124 136 152 172 184 200 220 232]; % second order
 fit = rational(sc);
-[m,n] = ind2sub([1,25],find(eval >= 2*fit.NumPoles,1));
+[m,n] = ind2sub([1,25],find(eval >= 8*fit.NumPoles,1));
 sz = [m,n,1,m*n];
 disp(['Automatic Grid Size: ' num2str(m) 'x' num2str(n)]);
-
 networks = fpg(sz);
 nvar = 3*sum(networks.^2+networks)/2;
    
 obj = @(x)synthobj(x,sz,freqs,err);
 constr = @(x)synconstr(x,sz);
 
-ub = 1;
-lb = -1;
+ub = 10;
+lb = -10;
 x = (ub-lb)*rand(1, nvar)+lb;
 ub = ub*ones(1, nvar);
 lb = -ub;
 
 %% fmincon optimization
 options = optimoptions("fmincon","Display","iter","UseParallel",true,...
-    "MaxFunctionEvaluations",2.5e4,"MaxIterations",3e9,...
-    "PlotFcn",["optimplotx","optimplotfirstorderopt","optimplotfvalconstr"],...
-    "EnableFeasibilityMode",false,"SubproblemAlgorithm","factorization");
+    "MaxFunctionEvaluations",2.5e5,"MaxIterations",3e9,...
+    "PlotFcn",["optimplotx","optimplotfirstorderopt","optimplotfvalconstr"]);
 tic
-[solution,fval,flag,output] = fmincon(obj,x,[],[],[],[],lb,ub,constr,options);
+[solution,fval,flag,output] = fmincon(obj,x,[],[],[],[],lb,ub,[],options);
 toc
 
-%% Global solution finder (maybe?)
-% options = optimoptions("fmincon","UseParallel",true,"SubproblemAlgorithm","cg",...
-%     "EnableFeasibilityMode",true,"StepTolerance",1e-7,"MaxIterations",70,...
-%     "MaxFunctionEvaluations",8e3);
-% gs = GlobalSearch("Display","iter","PlotFcn","gsplotbestf",...
-%     "StartPointsToRun","bounds","FunctionTolerance",1e-4);
-% prob = createOptimProblem("fmincon","lb",lb,"ub",ub,"nonlcon",constr,...
-%     "objective",obj,"options",options,"x0",x);
-% solution = run(gs,prob);
-
-
+%%
+% options = optimoptions("lsqcurvefit","Display","iter","UseParallel",true,...
+%     "MaxFunctionEvaluations",5e5,"PlotFcn","optimplotfval","MaxIterations",1e3,...
+%     "SubproblemAlgorithm","cg");
+% obj = @(x,xdata) synthobjcurve(x,sz,xdata);
+% solution = lsqcurvefit(obj,x,freqs,err,lb,ub,[],[],[],[],[],options);
 %% Plot output
 G = tranmake(solution, sz);
 H = tsc(G, sz);
 Y = sparameters(freqresp(H,freqs),freqs);
+% Y = best_optimal;
 
 figure('Name','Real Results','NumberTitle','off');
 for ii = 1:4
@@ -127,12 +163,12 @@ sgtitle('Comparison of Magnitudes');
 % end
 % sgtitle('Grid Networks');
 
-for ii = 1:m*n
-    figure('Name',['Network ' num2str(ii)],'NumberTitle','off');
-    F = sparameters(freqresp(G{ii},freqs),freqs);
-    rfplot(F); title(['Network ' num2str(ii)]);
-    xlim(xlims); ylim(ylims);
-end
+% for ii = 1:m*n
+%     figure('Name',['Network ' num2str(ii)],'NumberTitle','off');
+%     F = sparameters(freqresp(G{ii},freqs),freqs);
+%     rfplot(F); title(['Network ' num2str(ii)]);
+%     xlim(xlims); ylim(ylims);
+% end
 
 %% Functions
 function gsys = tranmake(x, sz)
@@ -166,12 +202,14 @@ function f = synthobj(x, sz, freqs, err)
     G = tranmake(x, sz);
     H = tsc(G, sz);
     F = sparameters(freqresp(H,freqs),freqs);
-    U = [real(squeeze(F.Parameters(1,1,:)))'...
-         real(squeeze(F.Parameters(1,2,:)))'...
-         real(squeeze(F.Parameters(2,2,:)))'...
-         imag(squeeze(F.Parameters(1,1,:)))'...
-         imag(squeeze(F.Parameters(1,2,:)))'...
-         imag(squeeze(F.Parameters(2,2,:)))'];
+    % U = [real(squeeze(F.Parameters(1,1,:)))'...
+    %      real(squeeze(F.Parameters(1,2,:)))'...
+    %      real(squeeze(F.Parameters(2,2,:)))'...
+    %      imag(squeeze(F.Parameters(1,1,:)))'...
+    %      imag(squeeze(F.Parameters(1,2,:)))'...
+    %      imag(squeeze(F.Parameters(2,2,:)))'];
+    U = [real(squeeze(F.Parameters(1,2,:)))'...
+         imag(squeeze(F.Parameters(1,2,:)))'];
     f = rmse(U,err);
 end
 
@@ -179,12 +217,14 @@ function f = synthobjcurve(x, sz, freqs)
     G = tranmake(x, sz);
     H = tsc(G, sz);
     F = sparameters(freqresp(H,freqs),freqs);
-    U = [real(squeeze(F.Parameters(1,1,:)))'...
-         real(squeeze(F.Parameters(1,2,:)))'...
-         real(squeeze(F.Parameters(2,2,:)))'...
-         imag(squeeze(F.Parameters(1,1,:)))'...
-         imag(squeeze(F.Parameters(1,2,:)))'...
-         imag(squeeze(F.Parameters(2,2,:)))'];
+    % U = [real(squeeze(F.Parameters(1,1,:)))'...
+    %      real(squeeze(F.Parameters(1,2,:)))'...
+    %      real(squeeze(F.Parameters(2,2,:)))'...
+    %      imag(squeeze(F.Parameters(1,1,:)))'...
+    %      imag(squeeze(F.Parameters(1,2,:)))'...
+    %      imag(squeeze(F.Parameters(2,2,:)))'];
+    U = [real(squeeze(F.Parameters(1,2,:)))'...
+         imag(squeeze(F.Parameters(1,2,:)))'];
     f = U;
 end
 
--- src/Synthesisv2/testsheet.m
+++ src/Synthesisv2/testsheet.m
@@ -1,6 +1,65 @@
-s = tf('s');
+%% Arbitrary Network
+close; clear testsheet; clc;
+nfreqs = 200;
+freqs = linspace(0,1,nfreqs);
+magpart = linspace(0,1,nfreqs);
+phasepart = cos(2*pi*freqs+deg2rad(-90));
+s11 = magpart.*exp(1j*phasepart);
+s22 = s11;
+s12 = sqrt(1-magpart.^2).*exp(1j*(phasepart-pi/2));
+s21 = s12;
+s = zeros(2,2,nfreqs);
+s(1,1,:) = s11;
+s(1,2,:) = s12;
+s(2,1,:) = s21;
+s(2,2,:) = s22;
+xlims = [0, 1];
+ylims = [-70, 10];
+sc = sparameters(s,freqs);
+figure('Name','Cascaded Network'); subplot(2,2,1);
+plot(freqs, real(squeeze(sc.Parameters(1,1,:))), freqs, real(squeeze(sc.Parameters(1,2,:))), ...
+     freqs, real(squeeze(sc.Parameters(2,1,:))), freqs, real(squeeze(sc.Parameters(2,2,:)))); grid on;
+title('Real Component'); xlabel('Frequency (Hz)'); ylabel('Magnitude'); xlim(xlims);
+subplot(2,2,2);
+plot(freqs, imag(squeeze(sc.Parameters(1,1,:))), freqs, imag(squeeze(sc.Parameters(1,2,:))), ...
+     freqs, imag(squeeze(sc.Parameters(2,1,:))), freqs, imag(squeeze(sc.Parameters(2,2,:)))); grid on;
+title('Imaginary Component'); xlabel('Frequency (Hz)'); ylabel('Magnitude'); xlim(xlims);
+subplot(2,2,[3,4]);
+plot(freqs, db(squeeze(sc.Parameters(1,1,:))), freqs, db(squeeze(sc.Parameters(1,2,:))), ...
+     freqs, db(squeeze(sc.Parameters(2,1,:))), freqs, db(squeeze(sc.Parameters(2,2,:)))); grid on;
+xlabel('Frequency (Hz)'); ylabel('Magnitude (dB)'); xlim(xlims); ylim(ylims);
+legend('S_1_1', 'S_1_2', 'S_2_1', 'S_2_2');
+sgtitle('Desired Network'); drawnow;
 
-H = 1.984e27*s^3/(s^6+2.513e9*s^5+1.216e20*s^4+2.004e29*s^3+4.8e39*s^2+3.917e48*s+6.153e58);
-freqs = linspace(5e9, 8e9, 200);
-f = freqresp(H, freqs);
-plot(freqs, db(squeeze(f(1,1,:))));
+%%
+c = circuit();
+add(c, [1 2], capacitor(0.45));
+add(c, [2 3], inductor(2.25));
+add(c, [3 4], capacitor(1));
+add(c, [2 3], resistor(0.1));
+add(c, [2 5], inductor(0.5));
+add(c, [3 5], capacitor(0.25));
+add(c, [3 5], inductor(0.75));
+add(c, [1 5], capacitor(0.01));
+add(c, [1 4], inductor(0.95));
+setports(c, [1 4], [3 5]);
+freqs = linspace(0.1, 1, 200);
+sc = sparameters(c, freqs);
+sc.Parameters(1,2,:) = sc.Parameters(1,2,:)*1.3e1;
+sc.Parameters(2,1,:) = sc.Parameters(2,1,:)*1.3e1;
+figure('Name','Cascaded Network'); subplot(2,2,1);
+plot(freqs, real(squeeze(sc.Parameters(1,1,:))), freqs, real(squeeze(sc.Parameters(1,2,:))), ...
+     freqs, real(squeeze(sc.Parameters(2,1,:))), freqs, real(squeeze(sc.Parameters(2,2,:)))); grid on;
+title('Real Component'); xlabel('Frequency (Hz)'); ylabel('Magnitude'); xlim(xlims);
+subplot(2,2,2);
+plot(freqs, imag(squeeze(sc.Parameters(1,1,:))), freqs, imag(squeeze(sc.Parameters(1,2,:))), ...
+     freqs, imag(squeeze(sc.Parameters(2,1,:))), freqs, imag(squeeze(sc.Parameters(2,2,:)))); grid on;
+title('Imaginary Component'); xlabel('Frequency (Hz)'); ylabel('Magnitude'); xlim(xlims);
+subplot(2,2,[3,4]);
+plot(freqs, db(squeeze(sc.Parameters(1,1,:))), freqs, db(squeeze(sc.Parameters(1,2,:))), ...
+     freqs, db(squeeze(sc.Parameters(2,1,:))), freqs, db(squeeze(sc.Parameters(2,2,:)))); grid on;
+xlabel('Frequency (Hz)'); ylabel('Magnitude (dB)'); xlim(xlims); ylim(ylims);
+legend('S_1_1', 'S_1_2', 'S_2_1', 'S_2_2');
+sgtitle('Desired Network'); drawnow;
+
+%%
